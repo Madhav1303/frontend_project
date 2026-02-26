cb7393d3dbd43df85c2ea458fafd0cfc47cc339e
Madhav1303 <madhavkishore.ks@gmail.com>
2026-02-26 08:55:44 +0530

    removed unwanted comments and improved code readability

--- app.js
+++ app.js
@@ -1,93 +1,62 @@
-function validate_signup()
-{
-let name = document.getElementById("name").value;
-if(validateEmail() && validatePassword() && name) 
-    alert("Signup Successfull");
-else
-alert("Invalid input");
-}
-
-function validate_login()
-{
-if(validateEmail() && validatePassword()) 
-    alert("Login Successfull");
-else
-alert("Invalid input");
-}
-
-function validateEmail()
-{
-var email = document.getElementById("email").value; 
-if(email.includes("@") && email.includes(".com"))
-return true; 
-else
-return false;
-}
-
-function validatePassword()
-{
-/*var pass = document.getElementById("password").value; 
-if(pass.length < 8)
-return false 
-else
-return true;*/
-const pass = document.getElementById("password").value;
-   let number_capital_letters = 0 ,number_small_letters = 0 , number_digits = 0;
-   let number_special_characters = 0;
-   let flag = true;
-   if(pass.length <8)
-   {
-    alert('The number of characters must be minimum 8');
-    flag = false;
-    return false;
-   }
-   for(let i = 0;i<pass.length;i++)
-   {
-    if(pass[i]>='a' && pass[i]<='z')
-        number_small_letters++;
-    else if(pass[i]>='A' && pass[i]<='Z')
-        number_capital_letters++;
-    else if(pass[i]>='0' && pass[i]<='9')
-        number_digits++;
+function validate_signup() {
+    let name = document.getElementById("name").value;
+    if (validateEmail() && validatePassword() && name)
+        alert("Signup Successfull");
     else
-        number_special_characters++;
-   }
-   if(number_capital_letters >=1 && number_small_letters >=1 && number_digits >=1 && number_special_characters >=1)
-    {
-      flag = true;
-    }
-   else{
-    if(number_capital_letters <1)
-       alert('Minimum number of capital letters must be 1');
-    if(number_small_letters <1)
-       alert('Minimum number of small letters must be 1');
-    if(number_digits <1)
-       alert('Minimum number of digits must be 1');
-    if(number_special_characters <1)
-       alert('Minimum number of special characters must be 1');
-    flag = false;
-   }
-   if(flag)
-    return true;
-   else
-    return false;
+        alert("Invalid input");
 }
 
-/*
-function validatePhoneNo()
-{
-var phone_no = document.getElementById("number").value; 
-flag = false
-if(phone_no.length == 10) 
-flag = true
-else
-flag = false
-for(var i = 0;i<phone_no.length;i++)
-{
-if(!(phone_no[i]>='0' && phone_no[i]<='9')) flag = false
+function validate_login() {
+    if (validateEmail() && validatePassword())
+        alert("Login Successfull");
+    else
+        alert("Invalid input");
 }
-if(flag) return true 
-else
-return false
+
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    if (email.includes("@") && email.includes(".com"))
+        return true;
+    else
+        return false;
 }
-*/
+
+function validatePassword() {
+    const pass = document.getElementById("password").value;
+    let number_capital_letters = 0, number_small_letters = 0, number_digits = 0;
+    let number_special_characters = 0;
+    let flag = true;
+    if (pass.length < 8) {
+        alert('The number of characters must be minimum 8');
+        flag = false;
+        return false;
+    }
+    for (let i = 0; i < pass.length; i++) {
+        if (pass[i] >= 'a' && pass[i] <= 'z')
+            number_small_letters++;
+        else if (pass[i] >= 'A' && pass[i] <= 'Z')
+            number_capital_letters++;
+        else if (pass[i] >= '0' && pass[i] <= '9')
+            number_digits++;
+        else
+            number_special_characters++;
+    }
+    if (number_capital_letters >= 1 && number_small_letters >= 1 && number_digits >= 1 && number_special_characters >= 1) {
+        flag = true;
+    }
+    else {
+        if (number_capital_letters < 1)
+            alert('Minimum number of capital letters must be 1');
+        if (number_small_letters < 1)
+            alert('Minimum number of small letters must be 1');
+        if (number_digits < 1)
+            alert('Minimum number of digits must be 1');
+        if (number_special_characters < 1)
+            alert('Minimum number of special characters must be 1');
+        flag = false;
+    }
+    if (flag)
+        return true;
+    else
+        return false;
+}
